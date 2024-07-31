683591eab86cd7e79ce7a3989afd1ab74c90f12f
RITIK SHARMA <ritiksharma555598@gmail.com>
2024-07-31 19:04:30 +0530

    manage all the folder

--- COMPONENTS AND PROPES AND STATES-2/Common Mistakes/codingprecties_9/src/App.js
+++
@@ -1,7 +0,0 @@
-// import Counter from './components/Counter'
-import Counter from './Components/Counter'
-import './App.css'
-
-const App = () => <Counter />
-
-export default App
--- COMPONENTS AND PROPES AND STATES-2/Common Mistakes/codingprecties_9/src/AppFruitCounter.js
+++
@@ -1,6 +0,0 @@
-// import FruitsCounter from './components/FruitsCounter'
-import FruitsCounter from './Components/FruitCounter'
-import './App.css'
-const AppFruitCounter = () => <FruitsCounter />
-
-export default AppFruitCounter;
