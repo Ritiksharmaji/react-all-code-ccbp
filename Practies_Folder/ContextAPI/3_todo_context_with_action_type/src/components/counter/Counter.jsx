251fa0066579370f281f72a136eeb96a647a1052
Ritiksharmaji <ritiksharma555598@gmail.com>
2025-12-24 13:01:32 +0530

    changing dispatch function to setCounter function

--- Practies_Folder/ContextAPI/3_todo_context_with_action_type/src/components/counter/Counter.jsx
+++ Practies_Folder/ContextAPI/3_todo_context_with_action_type/src/components/counter/Counter.jsx
@@ -3,21 +3,39 @@ import { CounterContext } from "../../context/counter/CounterContext";
 import { changeCount, decrease, increase, increaseByFive, resetCount } from "../../context/counter/actions";
 
 export default function Counter() {
-  const { count, dispatch } = useContext(CounterContext);
+  const { count, setCounter } = useContext(CounterContext);
 
   return (
     <>
       <h2>Count: {count}</h2>
 
-    <button onClick={() => dispatch(increase())}>+1</button>
-    <button onClick={() => dispatch(decrease())}>-1</button>
-    <button onClick={() => dispatch(increaseByFive())}>+5</button>
+    <button onClick={() => setCounter(increase())}>+1</button>
+    <button onClick={() => setCounter(decrease())}>-1</button>
+    <button onClick={() => setCounter(increaseByFive())}>+5</button>
     {/* dynaic type */}
-    <button onClick={() => dispatch(changeCount(1))}>+1</button>
-    <button onClick={() => dispatch(changeCount(-1))}>-1</button>
-    <button onClick={() => dispatch(changeCount(5))}>+5</button>
-    <button onClick={() => dispatch(resetCount())}>Reset</button>
+    <button onClick={() => setCounter(changeCount(1))}>+1</button>
+    <button onClick={() => setCounter(changeCount(-1))}>-1</button>
+    <button onClick={() => setCounter(changeCount(5))}>+5</button>
+    <button onClick={() => setCounter(resetCount())}>Reset</button>
 
     </>
   );
+
+  //  const { count, dispatch } = useContext(CounterContext);
+
+  // return (
+  //   <>
+  //     <h2>Count: {count}</h2>
+
+  //   <button onClick={() => dispatch(increase())}>+1</button>
+  //   <button onClick={() => dispatch(decrease())}>-1</button>
+  //   <button onClick={() => dispatch(increaseByFive())}>+5</button>
+  //   {/* dynaic type */}
+  //   <button onClick={() => dispatch(changeCount(1))}>+1</button>
+  //   <button onClick={() => dispatch(changeCount(-1))}>-1</button>
+  //   <button onClick={() => dispatch(changeCount(5))}>+5</button>
+  //   <button onClick={() => dispatch(resetCount())}>Reset</button>
+
+  //   </>
+  // );
 }
--- Practies_Folder/ContextAPI/3_todo_context_with_action_type/src/context/counter/CounterProvider.jsx
+++ Practies_Folder/ContextAPI/3_todo_context_with_action_type/src/context/counter/CounterProvider.jsx
@@ -3,11 +3,23 @@ import { CounterContext } from "./CounterContext";
 import { counterReducer } from "./CounterReducer";
 
 export const CounterProvider = ({ children }) => {
-  const [count, dispatch] = useReducer(counterReducer, 0);
-
+  // Initialize useReducer with counterReducer and initial state of 0 for count
+  // here dispatch will be used to send actions to the reducer to update the count state
+  // so we return both count and dispatch in the context value
+  // can we chage the
+  const [count, setCounter] = useReducer(counterReducer, 0);
+  
   return (
-    <CounterContext.Provider value={{ count, dispatch }}>
+    <CounterContext.Provider value={{ count, setCounter }}>
       {children}
     </CounterContext.Provider>
   );
+
+  // const [count, dispatch] = useReducer(counterReducer, 0);
+
+  // return (
+  //   <CounterContext.Provider value={{ count, dispatch }}>
+  //     {children}
+  //   </CounterContext.Provider>
+  // );
 };
